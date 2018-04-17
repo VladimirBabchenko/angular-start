4b2a7780e907b7eeafc8aaa276d2fcb0053d4439
Vova <mgrinevitch@gmail.com>
2018-04-17 13:13:59 +0300

    ts

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,24 @@
 import Student from "./ts/student";
 import StudentsGroup from "./ts/students-group";
 
-// const Babchenko = new Student("Vova", "Babchenko", 1982);
-// const Sidorov = new Student("Sid", "Sidorov", 1982);
-// const Petrova = new Student("Lena", "Petrova", 1920);
+const Babchenko = new Student("Vova", "Babchenko", 1982);
+const Sidorov = new Student("Sid", "Sidorov", 1982);
+const Petrova = new Student("Lena", "Petrova", 1920);
 
-// Babchenko.mark = [10, 20, 30, 40];
-// Babchenko.present();
-// Babchenko.present();
-// Babchenko.present();
+Babchenko.mark = [10, 20, 30, 40];
+Babchenko.present();
+Babchenko.present();
+Babchenko.present();
 
-// Sidorov.mark = [90, 20, 10, 40];
-// Sidorov.present();
-// Sidorov.present();
-// Sidorov.absent();
+Sidorov.mark = [90, 20, 10, 40];
+Sidorov.present();
+Sidorov.present();
+Sidorov.absent();
 
-// Petrova.mark = [0, 20, 30, 40];
-// Petrova.present();
-// Petrova.present();
-// Petrova.present();
+Petrova.mark = [0, 20, 30, 40];
+Petrova.present();
+Petrova.present();
+Petrova.present();
 
-// const group = new StudentsGroup(Babchenko, Sidorov, Petrova);
-// console.log(group.performance("Sidorov"));
+const group = new StudentsGroup(Babchenko, Sidorov, Petrova);
+console.log(group.performance("Sidorov"));
--- src/ts/student.ts
+++ src/ts/student.ts
@@ -9,49 +9,49 @@ class Student {
     ) { }
 
     present(): void {
-        this._presence.length < 25 && this._presence.push(true)
-    };
+        this._presence.length < 25 && this._presence.push(true);
+    }
 
     absent(): void {
         this._presence.length < 25 && this._presence.push(false);
-    };
+    }
 
     set mark(mark: number | number[]) {
         Array.isArray(mark) ?
             this._marks = this._marks.concat(mark) :
             this._marks.push(mark);
-    };
+    }
 
     get marks() {
         return this._marks;
-    };
+    }
 
     get presence() {
         return this._presence;
-    };
+    }
 
     getCurrentAge(): number {
         return new Date().getFullYear() - this.yearBirth;
-    };
+    }
 
     getAverageMarks(): number {
         return this._marks.reduce((prev, next) =>
             prev + next) / this._marks.length;
-    };
+    }
 
     getAveragePresence(): number {
         return this._presence.filter(presence => presence).length / this._presence.length;
-    };
+    }
 
     summary(): string {
         const averageMark: number = this.getAverageMarks();
         const averagePresence: number = this.getAveragePresence();
         if (averageMark > 90 && averagePresence > 0.9)
-            return "Уйти как молодчинка!"
+            return "Уйти как молодчинка!";
         if (averageMark > 90 || averagePresence > 0.9)
-            return "Норм, но можно лучше"
-        return "Редиска!"
-    };
+            return "Норм, но можно лучше";
+        return "Редиска!";
+    }
 }
 
 export default Student;
