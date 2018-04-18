62c3167fffa8990475bbd4145ab5adabdcbc22d1
Vova <mgrinevitch@gmail.com>
2018-04-18 11:00:30 +0300

    lastts

--- src/ts/students-group.ts
+++ src/ts/students-group.ts
@@ -1,11 +1,11 @@
 import Student from "./student";
 
-interface IStudentGroup {
+interface IStudentsGroup {
     attendance(data: string | number): number;
     performance(data: string | number): number;
 }
 
-class StudentsGroup implements IStudentGroup {
+class StudentsGroup implements IStudentsGroup {
     private _students: Student[] = [];
 
     constructor(...args: Student[]) {
@@ -14,17 +14,17 @@ class StudentsGroup implements IStudentGroup {
 
     get students() {
         return this._students;
-    };
+    }
 
     sortStudentsByAverageMark(): Student[] {
         return this._students.sort((student1, student2) =>
-            student1.getAverageMarks() - student2.getAverageMarks()).reverse()
-    };
+            student1.getAverageMarks() - student2.getAverageMarks()).reverse();
+    }
 
     sortStudentsByPresence(): Student[] {
         return this._students.sort((student1, student2) =>
             student1.getAveragePresence() - student2.getAveragePresence()).reverse();
-    };
+    }
 
     attendance(data: string | number): number {
         const sortedStudents: Student[] = data && this.sortStudentsByPresence();
@@ -36,7 +36,7 @@ class StudentsGroup implements IStudentGroup {
         return typeof data === "number" && data < 25 &&
             Number((this._students.filter(student => student.presence[data - 1]).length /
                 this._students.length).toFixed(2));
-    };
+    }
 
     performance(data: string | number): number {
         const sortStudents: Student[] = data && this.sortStudentsByAverageMark();
@@ -46,9 +46,9 @@ class StudentsGroup implements IStudentGroup {
         if (student) return sortStudents.indexOf(student) + 1;
 
         return typeof data === "number" && Number((sortStudents.reduce((sum, student) => {
-            return sum + student.marks[data - 1]
+            return sum + student.marks[data - 1];
         }, 0) / this._students.length).toFixed(2));
-    };
+    }
 }
 
 export default StudentsGroup;
